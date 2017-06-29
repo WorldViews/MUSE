fd40ba32c7d8c3b07dfad71838f34283e2544c46
donkimber <donkimber@gmail.com>
2017-06-29 12:32:27 -0700

    changed screen specs to a JSON
    rather than set of variables.   This leads to less boilerplate, and also allows specs to be save/loaded as JSON files

--- src/const/screen.js
+++ src/const/screen.js
@@ -1,7 +1,20 @@
 import {Math} from 'three';
 
+/*
 export let R = 1.0;
 export let TH_LEN = Math.degToRad(140);
 export let TH_MIN = Math.degToRad(180) - TH_LEN / 2;
 export let PH_MIN = Math.degToRad(31);
 export let PH_LEN = Math.degToRad(49);
+*/
+
+// moved the degToRad into the function reading this
+// to improve readability of the spec.
+export let screen1 = {
+    R: 1.0,
+    PH_MIN: 32,
+    PH_LEN: 49,
+    TH_MIN: 110,
+    TH_LEN: 140
+};
+
