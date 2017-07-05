f336f3e00ff4f0040bfdae7f72f7cc35989f2285
donkimber <donkimber@gmail.com>
2017-07-05 12:02:03 -0700

    add game object to model loader

--- src/loadModels.js
+++ src/loadModels.js
@@ -1,11 +1,15 @@
 
 import loadCollada from './loadCollada'
 
-function loadModels(specs, scene)
+//function loadModels(specs, scene)
+function loadModels(specs, scene, game)
 {
     specs.forEach(spec => {
 	loadCollada(spec.path, spec).then((collada) => {
 	    scene.add(collada.scene);
+	    if (game && spec.name) {
+		game.models[spec.name] = collada;
+	    }
 	});
     });
 }
