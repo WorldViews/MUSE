5570f4e99a720c840e9fd76599b3406f648d2b53
donkimber <donkimber@gmail.com>
2017-07-07 18:38:47 -0700

    tweaked to support modules

--- src/loadBVH.js
+++ src/loadBVH.js
@@ -1,13 +1,16 @@
 
 import * as THREE from 'three';
 
-let loadBVH = function(game, bvh) {
+import {BVHLoader} from './BVHLoader';
+
+let loadBVH = function(game, name, bvh) {
     var scene = game.scene;
-    var loader = new THREE.BVHLoader();
+    //var loader = new THREE.BVHLoader();
+    var loader = new BVHLoader();
     loader.load( bvh, function( result ) {
         console.log("BVH: ", result);
         var dancer = new THREE.Object3D();
-        skeletonHelper = new THREE.SkeletonHelper( result.skeleton.bones[ 0 ] );
+        var skeletonHelper = new THREE.SkeletonHelper( result.skeleton.bones[ 0 ] );
         skeletonHelper.skeleton = result.skeleton; // allow animation mixer to bind to SkeletonHelper directly
 	skeletonHelper.material.depthTest = true;
         var boneContainer = new THREE.Group();
@@ -18,10 +21,14 @@ let loadBVH = function(game, bvh) {
         dancer.scale.x = 0.06;
         dancer.scale.y = 0.06;
         dancer.scale.z = 0.06;
+        //dancer.scale.x = 1.0;
+        //dancer.scale.y = 1.0;
+        //dancer.scale.z = 1.0;
         scene.add(dancer);
         // play animation
-        mixer = new THREE.AnimationMixer( skeletonHelper );
+        var mixer = new THREE.AnimationMixer( skeletonHelper );
         mixer.clipAction( result.clip ).setEffectiveWeight( 1.0 ).play();
+	game.models[name] = dancer;
     } );
 }
 
