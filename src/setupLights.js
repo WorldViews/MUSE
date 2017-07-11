8e14f4f242be0e18783924a9cb3963830f51e8bf
Derrick McMillen <mcmillen@fxpal.com>
2017-07-11 13:42:26 -0700

    Fixed planet material and lighting

--- src/setupLights.js
+++ src/setupLights.js
@@ -12,7 +12,8 @@ function addLight(game, spec)
         sphere = new THREE.SphereGeometry( 1, 16, 8 );
     }
     var color = spec.color || 0xffffff;
-    let light = new THREE.PointLight(color, 2, 50);
+    let distance = spec.distance || 50;
+    let light = new THREE.PointLight(color, 2, distance);
     var g = new THREE.Group();
     if (spec.name)
         g.name = spec.name;
@@ -30,11 +31,10 @@ function addLight(game, spec)
 
 function setupLights(game)
 {
-
-    addLight(game, {name: 'domeLight', color: 0xffffff, position: [0,9.5,0]});
     addLight(game, {name: 'light1', color: 0xffaaaa, position: [30, 15,-10]});
     addLight(game, {name: 'light2', color: 0xaaffaa, position: [30, 15,  5]});
     addLight(game, {name: 'light3', color: 0xaaaaff, position: [30, 15, -5]});
+    addLight(game, {name: 'sun', color: 0xffffff, position: [0, 1000, 0], distance: 5000});
 }
 
 export {addLight, setupLights};
