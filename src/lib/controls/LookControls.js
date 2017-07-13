1d1d146b33ff401137ea1a223539569fddf4174e
donkimber <donkimber@gmail.com>
2017-07-12 17:20:05 -0700

    upped the movement speed

--- src/lib/controls/LookControls.js
+++ src/lib/controls/LookControls.js
@@ -16,7 +16,7 @@ var LookControls = function ( object, domElement )
 
     this.enabled = true;
 
-    this.movementSpeed = 1.0;
+    this.movementSpeed = 3.0;
     //this.lookSpeed = 0.005;
     this.lookSpeed = 0.05;
 
