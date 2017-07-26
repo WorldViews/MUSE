f9a7b5bc6fb0e15ca0e37533501c72105648a943
donkimber <donkimber@gmail.com>
2017-07-26 11:51:21 -0700

    moved some stuff from explicity code calls to nodes in config

--- src/app.js
+++ src/app.js
@@ -4,8 +4,8 @@ import CMPController from './controllers/CMPController';
 import {PanoPortal0} from './lib/PanoPortal0';
 import {PanoPortal} from './lib/PanoPortal';
 import { CMPProgram } from './CMPProgram';
-import { DanceController } from './controllers/DanceController';
 import { Game } from './Game';
+import { Dancer } from './controllers/DanceController';
 import Marquee from './Marquee';
 import NavigationController from './controllers/NavigationController';
 import { Scripts } from './Scripts';
@@ -17,9 +17,10 @@ import WebVR from './lib/vr/WebVR';
 
 import { ViewManager } from './ViewManager';
 import { NetLink } from './NetLink';
-import loadModels from './loadModels';
-import { loadScreens, loadScreen } from './loadScreen';
-import { setupLights } from './setupLights';
+//import loadModels from './loadModels';
+import {Loader} from './Loader';
+import { loadScreens, loadScreen } from './Screens';
+import { addLight, setupLights } from './Lights';
 import setupMarquee from './setupMarquee';
 
 function getParameterByName(name) {
@@ -29,21 +30,56 @@ function getParameterByName(name) {
 
 let {degToRad} = THREE.Math;
 
+var SPECS = [
+    {  type: 'Group',  name: 'station' },
+    {  type: 'Group',  name: 'g2',
+       position: [200,0,0],
+       children: {type: 'Axes'}
+    },
+    {  type: 'Group',  name: 'g3',
+       position: [200,-500,0],
+       rotation: [0,0,0.2],
+       children: [
+           {  type: 'Axes', name: 'axis3',
+              visible: false
+           }
+       ]
+    },
+    {  type: 'Axes',   name: 'xyz' },
+    {  type: 'Model', name: 'platform',
+       parent: 'station',
+       path: 'models/PlayDomeSkp_v1.dae',
+       position: [0, 0, 0],
+       rotation: [0, degToRad(0), 0],
+       scale: 0.025
+    },
+    {   type: 'Model', name: 'bmw',
+        parent: 'station',
+        path: 'models/bmw/model.dae',
+        position: [0.2, 0, 1.6],
+        //rotation: [0, degToRad(90), 0],
+        rotation: [0, degToRad(0), 0],
+        scale: 0.020,
+        visible: false
+    },
+    {  type: 'Dancer', name: 'dancer',
+       scale: .1, visible: false
+    }
+]
+
 function start(config) {
-    let MODEL_SPECS = config.modelSpecs || [
+    let MODEL_SPECS = config.specs || [
         {
-            name: 'station'
+            type: 'Group', name: 'station',
         },
-        {
-            name: 'platform',
+        {   type: 'Model', name: 'platform',
             parent: 'station',
             path: 'models/PlayDomeSkp_v1.dae',
             position: [0, 0, 0],
             rotation: [0, degToRad(0), 0],
             scale: 0.025
         },
-        {
-            name: 'bmw',
+        {   type: 'Model', name: 'bmw',
             parent: 'station',
             path: 'models/bmw/model.dae',
             position: [0.2, 0, 1.6],
@@ -51,6 +87,12 @@ function start(config) {
             rotation: [0, degToRad(0), 0],
             scale: 0.020,
             visible: false
+        },
+        {   type: 'Screen', name: 'mainScreen',
+            parent: 'station', radius: 8.8,
+            path: 'videos/Climate-Music-V3-Distortion_HD_540.webm',
+            phiStart: 34, phiLength: 47,
+            thetaStart: 110, thetaLength: 140
         }
     ];
 
@@ -67,11 +109,11 @@ function start(config) {
 
     window.game = game;
     game.defaultGroupName = 'station';
-
     game.gss = new GSS.SpreadSheet();
 
     let cmpProgram = new CMPProgram(game);
-
+    game.setProgram(cmpProgram);
+    
     let solarSystemController = new SolarSystemController(game);
     let starsController = new StarsController(game.scene, [0, 0, 0]);
     let renderer = isVRWithFallbackControl ? game.renderer.getUnderlyingRenderer() : game.renderer;
@@ -93,10 +135,6 @@ function start(config) {
         game.registerController('navigation', navigationController);
     }
 
-    let dancer = new DanceController(game, {visible: false});
-    game.registerController('dancer', dancer);
-    cmpProgram.registerPlayer(dancer);
-
     game.registerController('stars', starsController);
     game.registerController('cmp', cmpController);
     game.registerController('solarSystem', solarSystemController);
@@ -114,8 +152,10 @@ function start(config) {
     game.addToGame(game.marquee, "marque1"); // cause it to get grouped properly
     setupMarquee(game);
 
-    loadModels(MODEL_SPECS, game);
-    loadScreens(game);
+    //loadModels(MODEL_SPECS, game);
+    game.loader = new Loader(game, MODEL_SPECS);
+    //loadModels(MODEL_SPECS, game);
+    //loadScreens(game);
 
     if (!isVRWithFallbackControl) {
         //game.portal0 = new PanoPortal0(game);
