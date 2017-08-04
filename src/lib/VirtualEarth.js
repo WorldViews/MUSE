15a8ba607b28150291c9b23282a1e6052d1596bb
donkimber <donkimber@gmail.com>
2017-08-04 12:38:46 -0700

    moved some code to BlobSet

--- src/lib/VirtualEarth.js
+++ src/lib/VirtualEarth.js
@@ -29,7 +29,46 @@ function ranVertex(rMin, rMax) {
     var x = rxy*Math.cos(theta);
     var y = rxy*Math.sin(theta);
     return new THREE.Vector3(x,y,z);
-};
+}
+
+function setNumBlobs(geom, n, posfun) {
+    var v = geom.vertices;
+    if (v.length > n) {
+        v.length = n;
+    }
+    else {
+        for (var i = v.length; i < n; i++ ) {
+            v.push(posfun());
+        }
+    }
+    geom.vertices = v;
+    geom.verticesNeedUpdate = true;
+}
+    
+class BlobSet {
+    constructor(opts, ranVertexFun) {
+
+        opts = opts || {};
+        var n = opts.n || 1000;
+        var size = opts.size || 20;
+        var color = opts.color || 0xff0000;
+        var opacity = opts.opacity || 0.3;
+        this.geometry = new THREE.Geometry();
+	var sprite = null;
+        var texPath = opts.texture || "textures/sprites/disc.png";
+	sprite = new THREE.TextureLoader().load( texPath );
+        var rMin = opts.rMin || this.radius*1.1;
+        var rMax = opts.rMax || this.radius*1.2;
+        setNumBlobs(this.geometry, n, ranVertexFun);
+	this.material = new THREE.PointsMaterial( { size: size, sizeAttenuation: false,
+                                                   map: sprite,
+                                                   color: color,
+                                                   opacity: 0.3,
+                                                   alphaTest: 0.1,
+                                                   transparent: true } );
+        this.particles = new THREE.Points( this.geometry, this.material );
+    }
+}
 
 class Planet {
 
@@ -123,36 +162,13 @@ class Planet {
 
     addBlobs(opts)
     {
-        opts = opts || {};
-        var n = opts.n || 1000;
-        var size = opts.size || 20;
-        var color = opts.color || 0xff0000;
-        var opacity = opts.opacity || 0.3;
-        var geometry = new THREE.Geometry();
-	var sprite = null;
-        var texPath = opts.texture || "textures/sprites/disc.png";
-	sprite = new THREE.TextureLoader().load( texPath );
         var rMin = opts.rMin || this.radius*1.1;
         var rMax = opts.rMax || this.radius*1.2;
-        this.setNumBlobs(geometry, n, () => ranVertex(rMin, rMax));
-	var material = new THREE.PointsMaterial( { size: size, sizeAttenuation: false,
-                                                   map: sprite,
-                                                   color: color,
-                                                   opacity: 0.3,
-                                                   alphaTest: 0.1,
-                                                   transparent: true } );
-	//material.color.setHSL( 1.0, 0.3, 0.7 );
-        var particles = new THREE.Points( geometry, material );
-	this.group.add( particles );
-        return {geometry, material, particles};
+        var blobSet = new BlobSet(opts, () => ranVertex(rMin, rMax));
+        this.group.add(blobSet.particles);
+        return blobSet;
     }
 
-    setNumBlobs(geom, n, posfun) {
-	for (var i = 0; i < n; i++ ) {
-            geom.vertices.push(posfun());
-	}
-    }
-    
     update() {
         if (this.satTracker)
             this.satTracker.update();
@@ -216,15 +232,35 @@ class DataViz {
         var res = this.earth.addBlobs({n: 5000, size: 5, color: 0xFF0000, opacity: 1,
                                        rMin: this.rMin, rMax: this.rMax});
         this.energInMat = res.material;
-        this.energyInBlobs = res.particles;
+        this.eInBlobs = res.particles;
         var res = this.earth.addBlobs({n: 5000, size: 5, color: 0xFFFF00, opacity: 1,
                                        rMin: this.rMin, rMax: this.rMax});
         this.energOutMat = res.material;
-        this.energyOutBlobs = res.particles;
+        this.eOutBlobs = res.particles;
         this.d2Min = this.rMin*this.rMin;
         this.d2Max = this.rMax*this.rMax;
     }
-    
+
+    // We try just changing vertices and indicating needs update, but it doesn't work.
+    setEnergyXXX(n) {
+        var rMin = this.rMin;
+        var rMax = this.rMax;
+        var geo = this.eOutBlobs.geometry;
+        setNumBlobs(this.eOutBlobs.geometry, n, () => ranVertex(this.rMin, this.rMax));
+        geo.vertices = geo.vertices;
+        this.eOutBlobs.geometry = null;
+        this.eOutBlobs.geometry = geo;
+    }
+
+    setEnergy(n) {
+        var material, particles;
+        this.earth.group.remove(this.eOutBlobs);
+        var res = this.earth.addBlobs({n: n, size: 5, color: 0xFFFF00, opacity: 1,
+                                       rMin: this.rMin, rMax: this.rMax});
+        this.energOutMat = res.material;
+        this.eOutBlobs = res.particles;
+    }
+
     update() {
         if (this.co2Blobs)
             this.co2Blobs.rotation.y += 0.005;
@@ -238,16 +274,16 @@ class DataViz {
                 this.co2Density = 0.3;
             this.co2Mat.opacity = this.co2Density;
         }
-        if (this.energyInBlobs) {
+        if (this.eInBlobs) {
             var s0 = this.rMax/this.rMin;
             var s1 = .99;
-            this.scaleVertices(this.energyInBlobs.geometry,
+            this.scaleVertices(this.eInBlobs.geometry,
                                d2 => d2 < this.d2Min ? s0 : s1);
         }
-        if (this.energyOutBlobs) {
+        if (this.eOutBlobs) {
             var s0 = this.rMin/this.rMax;
             var s1 = 1/0.99;
-            this.scaleVertices(this.energyOutBlobs.geometry,
+            this.scaleVertices(this.eOutBlobs.geometry,
                                d2 => d2 > this.d2Max ? s0 : s1);
         }
     }
