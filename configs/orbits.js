b0947dc984e81c2ff878128b2abf9b46c513c860
donkimber <donkimber@gmail.com>
2017-08-18 10:34:27 -0700

    added UI

--- configs/orbits.js
+++ configs/orbits.js
@@ -2,13 +2,14 @@
 CONFIG = {
     'cameraControls': 'Orbit',
     'specs': [
+        {  type: 'ReactControls' },
         {  type: 'Stars' },
         {  type: 'PointLight', name: 'sun', position: [-1000, 0, 0], distance: 5000},
         {  type: 'PointLight', name: 'sun', position: [3000, 1000, 0], distance: 5000},
         {  type: 'PointLight', name: 'sun', position: [3000, -1000, 0], distance: 5000},
         {  type: 'VirtualEarth', name: 'vEarth',
            radius: 1.25, rot: [0,0,0],
-           satellites: 0, satTracks: 1,
+           satTracks: 1,
            dataViz: 0
         }
     ]
