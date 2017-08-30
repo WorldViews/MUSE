aa9fa4cad6161b1e2e192ac9512aa31bd4abe1a1
donkimber <donkimber@gmail.com>
2017-08-30 12:53:06 -0700

    moved satellite configs into configs/objects

---
+++ configs/objects/Iridium.js
@@ -0,0 +1,21 @@
+
+var LIGHTS = [
+    {  type: 'PointLight', name: 'light1', color: 0xffaaaa, position: [0, -25,-2]},
+    {  type: 'PointLight', name: 'light2', color: 0xaaffaa, position: [0, -25, 0]},
+    {  type: 'PointLight', name: 'light3', color: 0xaaaaff, position: [0, -25, 2]},
+    {  type: 'PointLight', name: 'sun',    color: 0xffffff, position: [0, 1000, 0], distance: 5000},
+];
+
+
+CONFIG = {
+    'cameraControls': 'Orbit',
+    'specs': [
+        {   type: 'Model', name: 'Juno',
+            parent: 'station',
+            path: 'models/satellites/Iridium/model.dae',
+            //path: 'models/ISS/NASA/Interior/Tex/ISS_Interior_USOnly_Tex.dae',
+            scale: 0.01
+        },
+        LIGHTS
+    ]
+};
