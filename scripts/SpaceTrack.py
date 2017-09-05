53e0c7fd625b63b5a5ebbb47d85044255f690c27
donkimber <donkimber@gmail.com>
2017-09-05 10:15:26 -0700

    setup for scraping archive

--- scripts/SpaceTrack.py
+++ scripts/SpaceTrack.py
@@ -5,6 +5,13 @@ import spacetrack.operators as op
 from spacetrack import SpaceTrackClient
 import SPACE_TRACK_AUTH as STA
 
+DIR = "STDB"
+
+def verifyDir(path):
+    if os.path.exists(path):
+        return
+    print "Creating directory",path
+    os.path.mkdir(path)
 
 def getJSON():
     st = SpaceTrackClient(identity=STA.identity, password=STA.password)
@@ -32,7 +39,44 @@ def getTLE_latest():
     file("9_1_2017.tle_latest.txt", "w").write(buf)
     #buf = st.tle_latest(ordinal=1, norad_cat_id=25544, orderby='epoch desc', limit=10, format='json')
 
+class SpaceTrackDB:
+    def __init__(self):
+        self.st = SpaceTrackClient(identity=STA.identity, password=STA.password)
+        self.dir = DIR
+
+    def fetch(self):
+        years = [1950, 1960, 1970, 1980, 1990, 2000, 2010]
+        months = [1]
+        days = [1]
+        for year in years:
+            for month in months:
+                for day in days:
+                    epoch = "%4d-%02d-%02d" % (year, month, day)
+                    self.getTLE(epoch)
+
+    def getTLE(self, epoch='2010-03-01', format='json'):
+        print "Getting data for", epoch
+        path = os.path.join(self.dir, "%s.tle_latest.%s" % (epoch, format))
+        path = path.replace("\\", "/")
+        if os.path.exists(path):
+            buf = file(path).read()
+            if format == 'json':
+                return json.loads(buf)
+            return buf
+        st = SpaceTrackClient(identity=STA.identity, password=STA.password)
+        #st = SpaceTrackClient(identity="donkimber@gmail.com", password="spacetrack12345")
+        buf = self.st.tle_latest(ordinal=1, orderby='epoch desc', format=format, epoch='>'+epoch)
+        if format == 'json':
+            obj = json.loads(buf)
+            buf = json.dumps(obj, indent=3)
+        print buf
+        file(path, "w").write(buf)
+        return buf
+
 if __name__ == '__main__':
     #getJSON()
-    getTLE()
+    #getTLE()
     #getTLE_latest()
+    stdb = SpaceTrackDB()
+    stdb.fetch()
+    #stdb.getTLE('2010-03-01')
