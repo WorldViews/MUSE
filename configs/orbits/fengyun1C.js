79ba66d4c5dc41b723fad996f9d4feb36a76bf7c
jtan96 <jtman1226@gmail.com>
2017-09-21 13:33:36 -0700

    Added fengyun tles and DEB visualization

---
+++ configs/orbits/fengyun1C.js
@@ -0,0 +1,37 @@
+
+CONFIG = {
+    'cameraControls': 'Orbit',
+    'program': {
+       duration: 3*24*60*60,
+       //duration: 100*3600,
+       //startTime: 1504131722.726 - 100*24*3600
+       //startTime: 1504131722.726
+        startTime: '1/10/2007',
+        playSpeed: 100,
+        playTime: '1/11/2007 14:26:03'
+    },
+    'specs': [
+        {  type: 'JQControls' },
+        {  type: 'Stars' },
+        {  type: 'PointLight', name: 'sun', position: [-1000, 0, 0], distance: 5000},
+        {  type: 'PointLight', name: 'sun', position: [3000, 1000, 0], distance: 5000},
+        {  type: 'PointLight', name: 'sun', position: [3000, -1000, 0], distance: 5000},
+        {  type: 'VirtualEarth', name: 'vEarth',
+           radius: 1.25, rot: [0,0,0],
+           satTracks: {dataSet: 'fengyun1c.3le',
+                        models: {
+                            22675: {path:'models/satellites/ComSat/model.dae',
+                                           scale: .001},
+                            24946: {path:'models/satellites/Iridium/model.dae',
+                                          scale: .00005}
+                        }},
+           //satTracks: {dataSet: 'iridCosmosColl.json'},
+           //satTracks: {models: [
+           //    'models/satellites/ComSat/model.dae',
+           //    'models/satellites/ComSat2/model.dae']},
+           //satTracks: 1,
+           dataViz: 0,
+           atmosphere: {'name': 'CO2Viz', opacity: .1}
+        }
+    ]
+};
