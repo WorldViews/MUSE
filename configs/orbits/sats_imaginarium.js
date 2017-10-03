0bdd9d9d7321a7ff813316f890a2f6f4deed4808
donkimber <donkimber@gmail.com>
2017-10-03 15:41:55 -0700

    now works by including imaginarium venue

--- configs/orbits/sats_imaginarium.js
+++ configs/orbits/sats_imaginarium.js
@@ -1,115 +1,5 @@
 
 
-var LIGHTS = [
-    {  type: 'PointLight', name: 'light1', color: 0xffaaaa, position: [0, -25,-2]},
-    {  type: 'PointLight', name: 'light2', color: 0xaaffaa, position: [0, -25, 0]},
-    {  type: 'PointLight', name: 'light3', color: 0xaaaaff, position: [0, -25, 2]},
-    {  type: 'PointLight', name: 'sun',    color: 0xffffff, position: [0, 1000, 0], distance: 5000},
-];
-
-var OBJ_MODEL = {
-    type: 'Model', name: 'derrick', parent: 'station',
-    path: 'models/obj/derrick.obj',
-    position: [20, 0, 1.6], rot: [0, 0, 0], scale: 2.0
-};
-
-var VID_BUBBLES = [
-    {   type: 'Screen', name: 'vidBubble1', parent: 'station', radius: 0.4, position: [0,3.6,0],
-        path: 'videos/YukiyoCompilation.mp4',
-    },
-    // {   type: 'Screen', name: 'vidBubble2', parent: 'station', radius: 0.4, position: [1,3.6,0],
-    //     path: 'webrtc+http://localhost:8081',
-    // },
-];
-
-var SPECS = [
-    {  type: 'Stats', right: '-0px' },
-    {  type: 'JQControls' },
-    {  type: 'ViewManager', bookmarksUrl: 'data/imaginarium_bookmarks.json' },
-    //{  type: 'Scripts' },
-    {   type: 'Group', name: 'station'  },
-    {   type: 'Model', name: 'platform',
-        parent: 'station',
-        path: 'models/PlayDomeSkp_v1.dae',
-        position: [0, 0, 0],
-        rot: [0, 0, 0],
-        scale: 0.025
-    },
-    {   type: 'Model', name: 'iridium',
-        parent: 'station',
-        path: 'models/satellites/Iridium/model.dae',
-        position: [0.2, 0, 1.6],
-        rot: [0, 0, 0],
-        scale: 0.001,
-        visible: false
-    },
-    //VID_BUBBLES,
-    {   type: 'Screen', name: 'mainScreen',
-        parent: 'station', radius: 8.8,
-        path: 'videos/SpaceDebris.mp4',
-        phiStart: 34, phiLength: 47,
-        thetaStart: 110, thetaLength: 140
-    },
-    {   type: 'Screen', name: 'rightScreen',
-        parent: 'station', radius: 8.8,
-        //path: 'videos/Climate-Music-V3-Distortion_HD_540.webm',
-        path: 'assets/images/SpaceDebrisTalk/Slide1.PNG',
-        phiStart: 34, phiLength: 47,
-        thetaStart: 300, thetaLength: 60
-    },
-    {   type: 'Screen', name: 'leftScreen',
-        parent: 'station', radius: 8.8,
-        //path: 'videos/Climate-Music-V3-Distortion_HD_540.webm',
-        path: 'assets/images/SpaceDebrisTalk/Slide1.PNG',
-        phiStart: 40, phiLength: 30,
-        thetaStart: 30, thetaLength: 50
-    },
-    {  type: 'Marquee',    name: 'marquee1',
-       radius: 7,
-       phiStart: -50, phiLength: 100,
-       thetaStart: 65, thetaLength: 20
-    },
-    LIGHTS,
-    {  type: 'Dancer', name: 'dancer', visible: false },
-    {  type: 'CMPDataViz', name: 'cmp',
-       position: [0, 2, 0], rotation: [0, 0, 0], scale: [1.5, 1, 1.5],
-       visible: false
-    },
-    {  type: 'SolarSystem' },
-    {  type: 'Stars' },
-    {  type: 'SlidePlayer', name: 'slidePlayer', screenName: 'rightScreen',
-       records: { records: [
-           { t: '1959-1-1',   url: 'assets/images/SpaceDebrisTalk/Slide1.PNG'},
-           { t: '1980-1-1',   url: 'assets/images/SpaceDebrisTalk/Slide2.PNG'},
-           { t: '1990-1-1',   url: 'assets/images/SpaceDebrisTalk/Slide3.PNG'},
-           //{ t: '2000-1-1',   url: 'assets/images/SpaceDebrisTalk/Slide4.PNG'},
-           { t: '2000-1-1',   url: 'videos/YukiyoCompilation.mp4'},
-           { t: '2010-1-1',   url: 'assets/images/SpaceDebrisTalk/Slide5.PNG'},
-           { t: '2017-1-1',   url: 'assets/images/SpaceDebrisTalk/Slide6.PNG'}
-       ]}
-    },
-    {  type: 'SlidePlayer', name: 'slidePlayer2', screenName: 'leftScreen',
-       records: { records: [
-           { t: '2016-1-1',   url: 'assets/images/SpaceDebrisTalk/Slide6.PNG'},
-           { t: '2017-1-1',   url: 'assets/images/SpaceDebrisTalk/Slide7.PNG'},
-           { t: '2018-1-1',   url: 'assets/images/SpaceDebrisTalk/Slide8.PNG'}
-       ]}
-    },
-    //{  type: 'Hurricane', scale: 0.01 }
-    {  type: 'VirtualEarth', name: 'vEarth', satTracks: 0,
-       //radius: 1.25,
-       radius: 0.5,
-       position: [0,1.9,0],
-       satTracks: {dataSet: 'stdb/all_stdb.json',
-                    models: {
-                        22675: {path:'models/satellites/ComSat/model.dae',
-                                       scale: .001},
-                        24946: {path:'models/satellites/Iridium/model.dae',
-                                      scale: .00005}
-                    }},
-    }
-];
-
 function watchCollision(game)
 {
     game.program.setTimeRange('2/10/2009 8:40:00', '2/10/2009 12:40:00');
@@ -118,6 +8,7 @@ function watchCollision(game)
 
 function onStart(game)
 {
+    return;
     var t = game.program.getPlayTime();
     var t = 1234284990.0;
     var p = new THREE.Vector3(-1698.7030173737744, 1503.0165902867889, 6777.4862190372805);
@@ -133,9 +24,51 @@ function useWebWorker(game)
         satTracks.setWorkerRate(10);
 }
 
+VEARTH = {
+    type: 'VirtualEarth', name: 'vEarth', satTracks: 0,
+    radius: 1.25, position: [0,1.9,0],
+    satTracks: {dataSet: 'stdb/all_stdb.json',
+                models: {
+                    22675: {path:'models/satellites/ComSat/model.dae',
+                           scale: .001},
+                    24946: {path:'models/satellites/Iridium/model.dae',
+                          scale: .00005}
+                }},
+}
+
+SLIDE_PLAYERS = [
+    {  type: 'SlidePlayer', name: 'slidePlayerCenter', screenName: 'mainScreen',
+       records: { records: [
+           { t: '1950-1-1',   url: 'assets/images/SpaceDebrisTalk/Slide1.PNG'},
+           { t: '1960-1-1',   url: 'videos/SpaceDebris.mp4'},
+           { t: '2000-1-1',   url: 'assets/images/SpaceDebrisTalk/Slide2.PNG'},
+       ]}
+   },
+    {  type: 'SlidePlayer', name: 'slidePlayerLeft', screenName: 'leftScreen',
+       records: { records: [
+           { t: '2016-1-1',   url: 'assets/images/SpaceDebrisTalk/Slide6.PNG'},
+           { t: '2017-1-1',   url: 'assets/images/SpaceDebrisTalk/Slide7.PNG'},
+           { t: '2018-1-1',   url: 'assets/images/SpaceDebrisTalk/Slide8.PNG'}
+       ]}
+   },
+    {  type: 'SlidePlayer', name: 'slidePlayerRight', screenName: 'rightScreen',
+       records: { records: [
+           { t: '1959-1-1',   url: 'assets/images/SpaceDebrisTalk/Slide1.PNG'},
+           { t: '1980-1-1',   url: 'assets/images/SpaceDebrisTalk/Slide2.PNG'},
+           { t: '1990-1-1',   url: 'assets/images/SpaceDebrisTalk/Slide3.PNG'},
+           //{ t: '2000-1-1',   url: 'assets/images/SpaceDebrisTalk/Slide4.PNG'},
+           { t: '2000-1-1',   url: 'videos/YukiyoCompilation.mp4'},
+           { t: '2010-1-1',   url: 'assets/images/SpaceDebrisTalk/Slide5.PNG'},
+           { t: '2017-1-1',   url: 'assets/images/SpaceDebrisTalk/Slide6.PNG'}
+       ]}
+    },
+];
+
 CONFIG = {
     onStart: onStart,
     'cameraControls': 'JoelControls',
+    'ui': 'JQControls',
+    //'ui': {'type': 'ReactControls'},
     'program': {
        //duration: 32*60,
        duration: 65*365*24*60*60,
@@ -162,5 +95,7 @@ CONFIG = {
            //'Add Collision': addCollisions
        }
     },
-    'specs': SPECS
+    'venue': '/configs/venues/imaginarium.js',
+    //'venue': '/configs/imaginarium.js',
+    'specs': [VEARTH, SLIDE_PLAYERS]
 };
