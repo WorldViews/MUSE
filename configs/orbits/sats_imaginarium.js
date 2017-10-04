427d19aa45a1052b5fc5fb5576191829cfe9f487
donkimber <donkimber@gmail.com>
2017-10-04 14:47:39 -0700

    use new slides format

--- configs/orbits/sats_imaginarium.js
+++ configs/orbits/sats_imaginarium.js
@@ -36,23 +36,24 @@ VEARTH = {
                 }},
 }
 
-SLIDE_PLAYERS = [
-    {  type: 'SlidePlayer', name: 'slidePlayerCenter', screenName: 'mainScreen',
-       records: { records: [
+
+MEDIA_SPEC = [
+    {  type: 'Slides', group: 'mainScreen',
+       records: [
            { t: '1950-1-1',   url: 'assets/images/SpaceDebrisTalk/Slide1.PNG'},
            { t: '1960-1-1',   url: 'videos/SpaceDebris.mp4'},
            { t: '2000-1-1',   url: 'assets/images/SpaceDebrisTalk/Slide2.PNG'},
-       ]}
+       ]
    },
-    {  type: 'SlidePlayer', name: 'slidePlayerLeft', screenName: 'leftScreen',
-       records: { records: [
+    {  type: 'Slides', group: 'leftScreen',
+       records: [
            { t: '2016-1-1',   url: 'assets/images/SpaceDebrisTalk/Slide6.PNG'},
            { t: '2017-1-1',   url: 'assets/images/SpaceDebrisTalk/Slide7.PNG'},
            { t: '2018-1-1',   url: 'assets/images/SpaceDebrisTalk/Slide8.PNG'}
-       ]}
+       ]
    },
-    {  type: 'SlidePlayer', name: 'slidePlayerRight', screenName: 'rightScreen',
-       records: { records: [
+    {  type: 'Slides', group: 'rightScreen',
+       records: [
            { t: '1959-1-1',   url: 'assets/images/SpaceDebrisTalk/Slide1.PNG'},
            { t: '1980-1-1',   url: 'assets/images/SpaceDebrisTalk/Slide2.PNG'},
            { t: '1990-1-1',   url: 'assets/images/SpaceDebrisTalk/Slide3.PNG'},
@@ -60,9 +61,10 @@ SLIDE_PLAYERS = [
            { t: '2000-1-1',   url: 'videos/YukiyoCompilation.mp4'},
            { t: '2010-1-1',   url: 'assets/images/SpaceDebrisTalk/Slide5.PNG'},
            { t: '2017-1-1',   url: 'assets/images/SpaceDebrisTalk/Slide6.PNG'}
-       ]}
+       ]
     },
 ];
+
 
 CONFIG = {
     onStart: onStart,
@@ -93,9 +95,11 @@ CONFIG = {
            'Watch Collision': watchCollision,
            'Web Worker': useWebWorker,
            //'Add Collision': addCollisions
-       }
+       },
+       //media: MEDIA_SPEC
     },
     'venue': '/configs/venues/imaginarium.js',
     //'venue': '/configs/imaginarium.js',
-    'specs': [VEARTH, SLIDE_PLAYERS]
+    //'specs': [VEARTH, SLIDE_PLAYERS]
+    'specs': [VEARTH, MEDIA_SPEC]
 };
