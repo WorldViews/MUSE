34db8adf038172ddecd53de0abf1d77a9e5254e3
donkimber <donkimber@gmail.com>
2017-10-05 12:46:16 -0700

    reformat

--- src/lib/MediaControl.js
+++ src/lib/MediaControl.js
@@ -3,6 +3,7 @@
   with time.   The state at a given time is characterized by a 'record' which is
   just a JSON dictionary, which must have one field called 't', which is the time.
 */
+
 import {DynamicObject} from './DynamicObject';
 import {DynamicObjectDB} from './DynamicObjectDB';
 import {Game} from '../Game';
@@ -75,26 +76,12 @@ class SlidePlayer extends DynamicObjectDB
         this.dump();
     }
 
-    update(dt) {
-    }
-
-    play() {
-    }
-
-    pause() {
-    }
-
-    getPlayTime() {
-        return this.playTime;
-    }
-
-    setPlaySpeed(s) {
-        this.playSpeed = s;
-    }
-
-    getPlaySpeed() {
-        return this.playSpeed;
-    }
+    update(dt) {}
+    play() {}
+    pause() {}
+    getPlayTime() { return this.playTime; }
+    setPlaySpeed(s) { this.playSpeed = s; }
+    getPlaySpeed() { return this.playSpeed; }
 
     postMessage(msg)
     {
@@ -112,6 +99,48 @@ class SlidePlayer extends DynamicObjectDB
         console.log("*** SlidePlayer "+this.name+" postMessage: "+JSON.stringify(msg));
     }
 }
+
+class StagePlayer extends DynamicObjectDB
+{
+    constructor(game, options) {
+        var name = options.name;
+        var screenName = options.group;
+        super(name);
+        this.game = game;
+        this.screenName = screenName;
+        this.playSpeed = 1;
+        this.playTime = 0;
+        console.log("Screen "+screenName);
+        var recs = options.records;
+        recs = fixRecs(recs);
+        var recsObj = {records: recs};
+        this.addRecords(recsObj);
+        this.dump();
+    }
+
+    update(dt) {}
+    play() {}
+    pause() {}
+    getPlayTime() { return this.playTime; }
+    setPlaySpeed(s) { this.playSpeed = s; }
+    getPlaySpeed() { return this.playSpeed; }
+
+    postMessage(msg)
+    {
+        var ui = game.controllers.ui;
+        //msg['name'] = msg['id'];
+        if (msg['msgType'] != "v3d.delete") {
+            //msg['imageUrl'] = "http://"+serverHost+"/"+msg['imageUrl'];
+            var name = msg.name;
+            console.log("handle "+msg.msgType+" "+name);
+            if (ui)
+                ui.stageControl.selectModel(name);
+            else
+                console.log("StagePlayer: no UI");
+        }
+    }
+}
+
 
 
 function runSlideShow(game)
@@ -154,6 +183,23 @@ Game.registerNodeType("Slides", (game, options) => {
     if (!options.name)
         options.name = "slidePlayer_"+group;
     var slideShow = new SlidePlayer(game, options);
+    game.registerController(options.name, slideShow);
+    game.registerPlayer(slideShow);
+    return slideShow;
+});
+
+Game.registerNodeType("Stage", (game, options) => {
+    console.log("===========================")
+    console.log("slides ", options);
+    console.log("slides "+JSON.stringify(options));
+    var stage = options.stage;
+    if (!stage) {
+        Util.reportError("No stage specified");
+        return null;
+    }
+    if (!options.name)
+        options.name = ("stagePlayer_"+stage).replace(" ","_");
+    var slideShow = new StagePlayer(game, options);
     game.registerController(options.name, slideShow);
     game.registerPlayer(slideShow);
     return slideShow;
