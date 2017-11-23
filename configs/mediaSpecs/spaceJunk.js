0c7e5812d59d1912eed614fb8239a159824fce9e
donkimber <donkimber@gmail.com>
2017-11-22 18:28:21 -0800

    tweak formatting

--- configs/mediaSpecs/spaceJunk.js
+++ configs/mediaSpecs/spaceJunk.js
@@ -1,26 +1,28 @@
-
-MEDIA_SPEC = [
-   {  type: 'MediaSequence',
+var MEDIA_SPEC =
+[
+   {  type: "MediaSequence",
       records: [
-          { t: '1950-1-1',
-            mainScreen: {url: 'assets/images/SpaceDebrisTalk/Slide1.PNG'},
-            leftScreen: {url: 'assets/images/SpaceDebrisTalk/Slide1.PNG'},
-            controlScript: {note: "This is a note", n: 5},
+          { mainScreen: {"url": "assets/images/SpaceDebrisTalk/Slide1.PNG"},
+            leftScreen: {"url": "assets/images/SpaceDebrisTalk/Slide1.PNG"},
+            controlScript: {"note": "This is a note", "n": 5},
             year: {text: 1969}
           },
-          { t: '1980-1-1',
-            'mainScreen': { url: 'assets/images/SpaceDebrisTalk/Slide2.PNG'},
-            'leftScreen': { url: 'assets/images/SpaceDebrisTalk/Slide3.PNG'},
-            'controlScript': {note: "This is a second note", n: 25},
-            'year': { text: 1973}
+          { mainScreen: { "url": "assets/images/SpaceDebrisTalk/Slide2.PNG"},
+            leftScreen: { "url": "assets/images/SpaceDebrisTalk/Slide3.PNG"},
+            controlScript: {"note": "This is a second note", "n": 25},
+            year: { text: 1973}
           },
-          { mainScreen: {url: 'assets/images/SpaceDebrisTalk/Slide3.PNG'}},
-        //{ t: '2000-1-1',   url: 'assets/images/SpaceDebrisTalk/Slide4.PNG'},
-          { mainScreen: { url: 'videos/YukiyoCompilation.mp4'}},
-          { mainScreen: { url: 'assets/images/SpaceDebrisTalk/Slide5.PNG'}},
-          { mainScreen: { url: 'assets/images/SpaceDebrisTalk/Slide6.PNG'}}
+          { t: "1980-1-1",
+            mainScreen: { "url": "assets/images/SpaceDebrisTalk/Slide3.PNG"}},
+          { t: "1990-1-1",
+            mainScreen: { "url": "videos/2006_co2_flow_1024x512.mp4"}},
+          { t: "2000-1-1",
+            mainScreen: { "url": "assets/images/SpaceDebrisTalk/Slide5.PNG"}},
+          { t: "2010-1-1",
+            mainScreen: { "url": "assets/images/SpaceDebrisTalk/Slide6.PNG"}}
     ]
    },
-];
+   //{ type: 'foo' }
+]
 
 MUSE.returnValue(MEDIA_SPEC);
