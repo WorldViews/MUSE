937d2d71c2915d1fefbd0d8c9c50cdad55285b03
donkimber <donkimber@gmail.com>
2017-11-24 18:40:53 -0800

    made simpler module for adding video bubbles.

--- configs/programs/cmp_vidBubbles.js
+++ configs/programs/cmp_vidBubbles.js
@@ -1,14 +1,46 @@
 
+
 (function() {
 
-BUBBLES = [
-{   type: 'Screen', name: 'vidBubble3', parent: 'videoBubbles', radius: 0.6, position: [2,1,-7.8],
-    //path: 'assets/video/ISS_tour.mp4', autoPlay: false
-    side: "FrontSide",
-    rotation: [0,-1.6,0],
-    path: 'assets/video/StephanAndBill_4Kx2K.mp4', autoPlay: false
-},
-];
+var toRad = THREE.Math.degToRad;
+
+window.radialPos = function(angle, r, h) {
+    r = r || 7.4;
+    h = h || 1.0;
+    var x = -r*Math.cos(toRad(angle));
+    var z = -r*Math.sin(toRad(angle));
+    return [x,h,z];
+}
+
+var vidNames = [
+    "StephanAndBill_4Kx2K.mp4",
+    "ErikAndBill_4Kx2K.mp4",
+    "GreenlandMelting360_720p.mp4",
+    "ClimateChangeFiji360_1440.mp4"
+]
+
+function getBubbles(vidNames) {
+    var angle = 60;
+    var radius = 0.5;
+    var parent = 'videoBubbles';
+    var numBubbles = 0;
+    return vidNames.map(name => {
+        numBubbles++;
+        var path = "assets/video/"+name;
+        var bubbleName = "videoBubble" + numBubbles;
+        var bubble = {  type: 'Screen', parent, radius, path, name: bubbleName,
+            position: radialPos(angle),
+            side: "FrontSide", autoPlay: false,
+            rotation: [0,-1.6,0],
+        };
+        angle += 15;
+        //alert("bubble: "+JSON.stringify(bubble));
+        return bubble;
+    });
+}
+
+var BUBBLES = getBubbles(vidNames);
+//console.log("BUBBLES", BUBBLES);
 
 MUSE.returnValue(BUBBLES);
 
