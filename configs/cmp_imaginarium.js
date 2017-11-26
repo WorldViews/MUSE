c5fdf1f375cb5d67b8299a516e59e57ef9b74be3
donkimber <donkimber@gmail.com>
2017-11-25 16:10:29 -0800

    added code for updating CMPViz2 from state

--- configs/cmp_imaginarium.js
+++ configs/cmp_imaginarium.js
@@ -1,56 +1,104 @@
 
-/*
-MEDIA_SPECS = [
-    { type : 'MediaSequence',
-      records: [
-          //{ t: 0,    mainScreen:  {url: 'assets/images/SpaceDebrisTalk/Slide1.PNG'}},
-         // { t: 0,   mainScreen:  {url: 'assets/video/Climate-Music-V3-Distortion_HD_540.webm'}},
-          { t: 0,   mainScreen:  {url: 'assets/video/ClimateMusicProj-v7-HD.mp4'}},
-          { t: 50,   leftScreen:  {url: 'assets/images/SpaceDebrisTalk/Slide6.PNG'}},
-          { t: 100,  rightScreen: {url: 'assets/images/SpaceDebrisTalk/Slide1.PNG'}},
-          { t: 200,  leftScreen:  {url: 'assets/images/SpaceDebrisTalk/Slide7.PNG'},
-                     rightScreen: {url: 'assets/images/SpaceDebrisTalk/Slide2.PNG'}},
-          { t: 400,  leftScreen:  {url: 'assets/images/SpaceDebrisTalk/Slide8.PNG'}},
-          { t: 500,  rightScreen: {url: 'assets/video/YukiyoCompilation.mp4'}},
-          { t: 600,  rightScreen: {url: 'assets/images/SpaceDebrisTalk/Slide5.PNG'}},
-          { t: 700,  rightScreen: {url: 'assets/images/SpaceDebrisTalk/Slide6.PNG'}}
-      ]
-    },
-    {  type: 'StageStream', stage: 'Main Stage',
-       records: [
-           { t: 100,   name: 'none'},
-           { t: 200,   name: 'dancer'},
-           { t: 300,   name: 'cmp'},
-           { t: 1000,   name: 'dancer'}
-       ]
-    }
-];
-*/
+
 MEDIA_SPECS = "configs/mediaSpecs/cmp_showcase.js";
-/*
-VID_BUBBLES = [
-    {   type: 'Screen', name: 'vidBubble1', parent: 'station', radius: 0.4, position: [0,3.6,0],
-        path: 'http://192.168.16.206:8080',
-        imageType: 1
-    },
-]
-*/
 
 SCRIPTS = {  type: 'Scripts' };
 
+VEARTH = [
+    {  type: 'VirtualEarth', name: 'vEarth',
+       radius: 1.25, position: [0,1.9,0],
+       satellites: 0, satTracks: 0,
+       dataViz: 1,
+       atmosphere: {'name': 'CO2Viz', opacity: .1}
+    }
+];
+
+var SPECS = [
+    //{  type: 'JQControls' },
+    {  type: 'Group', name: 'station'  },
+//    {  type: 'CMPData' },
+    {  type: 'CMPDataViz', name: 'cmp',
+        position: [0, 1, 0],
+        //position: [-10, 0, 0],
+        rotation: [0, 0, 0],
+        //scale: [1.5, 1, 1.5],
+        scale: [1.2, 0.8, 1.2],
+        visible: true,
+        startTime: 10*60, // 10 minutes before starting
+        duration: 20*60   // 20 minutes duration time
+    },
+    //{  type: 'Stars' },
+    VEARTH
+];
+
 POSTERS = "configs/programs/cmp_posters.js";
 BUBBLES = "configs/programs/cmp_vidBubbles.js";
+
+function toHHMMSS(t) {
+    var sec_num = parseInt(t, 10); // don't forget the second param
+    var hours   = Math.floor(sec_num / 3600);
+    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+    var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+    if (hours > 0) {
+        return sprintf("%02d:%02d:%02d", hours, minutes, seconds);
+    } else {
+        return sprintf("%02d:%02d", minutes, seconds);
+    }
+}
+
+function updateCMPViz2(year)
+{
+    if (!year) {
+        return;
+    }
+    var gss = game.program.gss;
+    if (gss) {
+        var nar = gss.getFieldByYear(year, "narrative") || "";
+        if (nar) {
+            nar = Math.floor(year) + ':' + nar;
+        }
+        this.game.state.set('narrative', nar);
+    }
+    var earth = game.controllers.vEarth;
+    var atm = earth.atmosphere;
+    var startYear = 1800;
+    var endYear = 2300;
+    var co2Min = 280;
+    var co2Max = 1970;
+    var TMin = 13.0;
+    var TMax = 33.0;
+    var co2 = game.state.get("co2");
+    var T = game.state.get("temperature");
+    var balance = game.state.get("balance");
+    var co2f = (co2 - co2Min)/(co2Max - co2Min);
+    var Tf = (T - TMin)/(TMax - TMin);
+    var h = 0.6 + .6*Tf;
+    try {
+        console.log(sprintf("year: %s co2: %6.2f T: %6.2f balance: %6.2f  Tf: %4.2f h: %4.2f  co2f: %4.2f",
+                year, co2, T, balance, Tf, h, co2f));
+    } catch(e) {
+        console.log(e);
+    }
+    atm.setOpacity(0.9*co2f);
+    atm.setHue(h);
+}
+
+function onStartProgram() {
+    game.state.on("year", updateCMPViz2);
+}
 
 CONFIG = {
     //onStart: setupPosterActions,
     //'cameraControls': 'Orbit',
     'cameraControls': {type: 'MultiControls', movementSpeed: .15, keyPanSpeed: .01},
     //'cameraControls': 'JoelControls',
-    'webUI': {type: 'DATGUIControls',
-    // 'webUI': {type: 'JQControls',
+    //'webUI': {type: 'DATGUIControls',
+    'webUI': {type: 'JQControls',
            //screens: ["mainScreen"],
           },
     'program': {
+        onStartProgram: onStartProgram,
        duration: 32*60,
        gss: "https://spreadsheets.google.com/feeds/list/1Vj4wbW0-VlVV4sG4MzqvDvhc-V7rTNI7ZbfNZKEFU1c/default/public/values?alt=json",
        stages: [
@@ -67,15 +115,21 @@ CONFIG = {
            }
        ],
        channels: [
-           { name: 'time', default: 0 },
-           { name: 'year', min: 1850, max: 2300, default: 1850 },
-           'narrative',
-           'spaceStatus'
+           {name: 'time',        label: "Time",    format: toHHMMSS , default: 0},
+           {name: 'year',        label: "Year"                      , default: 1850, min: 1850, max: 2300 },
+           {name: 'temperature', label: "T",       format: "%6.2f"  },
+           {name: 'co2',         label: "CO2",     format: "%6.2f"  },
+           {name: 'balance',     label: "Balance", format: "%8.1f"  },
+           //'dyear',
+           'spacer',
+           {name: 'narrative', style: "height:110px", fieldElement: "div"}
         ],
-       media: MEDIA_SPECS
+
+       media: MEDIA_SPECS,
+       nodes: [SCRIPTS, POSTERS, BUBBLES, SPECS]
     },
     'venue': '/configs/venues/imaginarium.js',
-    'specs': [SCRIPTS, POSTERS, BUBBLES]
+    //'specs': [SCRIPTS, POSTERS, BUBBLES]
 };
 
 MUSE.returnValue(CONFIG);
