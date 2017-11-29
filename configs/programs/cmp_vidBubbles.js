8310663709ccb303f084e14c009f8e6f5aeef0b3
donkimber <donkimber@gmail.com>
2017-11-29 14:29:48 -0800

    Now video bubbles can have images and video
    if they have both, they show the image until the bubble is entered.

--- configs/programs/cmp_vidBubbles.js
+++ configs/programs/cmp_vidBubbles.js
@@ -2,25 +2,33 @@
 
 (function() {
 
-var vidNames = [
-    "StephanAndBill_4Kx2K.mp4",
+var vidSpecs = [
+    {  name: 'Stephan and Bill',
+       video: 'StephanAndBill_4Kx2K.mp4',
+       image: 'assets/images/FXPALBanner.jpg'
+    },
     "ErikAndBill_4Kx2K.mp4",
+    "webrtc://192.168.16.206:8080/"
     //"GreenlandMelting360_720p.mp4",
-    "GreenlandMelting360_3840p.mp4",
-    "ClimateChangeFiji360_1440.mp4"
+    //"GreenlandMelting360_3840p.mp4",
+    //"ClimateChangeFiji360_1440.mp4"
 ]
 
-function getBubbles(vidNames) {
+function getBubbles(vidSpecs) {
     var angle = 60;
     var radius = 0.5;
     game.getGroup('videoBubbles', {parent: 'station' });
     var parent = 'videoBubbles';
     var numBubbles = 0;
-    return vidNames.map(name => {
+    return vidSpecs.map(spec => {
+        if (typeof spec == "string")
+            spec = {name: spec, video: spec};
         numBubbles++;
-        var path = "assets/video/"+name;
+        var path = "assets/video/"+spec.video;
         var bubbleName = "videoBubble" + numBubbles;
         var bubble = {  type: "VideoBubble", parent, radius, path, name: bubbleName,
+            videoPath: path,
+            imagePath: spec.image,
             position: Util.radialPosition(angle),
             rotation: [0,-1.6,0],
         };
@@ -29,7 +37,7 @@ function getBubbles(vidNames) {
     });
 }
 
-var BUBBLES = getBubbles(vidNames);
+var BUBBLES = getBubbles(vidSpecs);
 //console.log("BUBBLES", BUBBLES);
 
 MUSE.returnValue(BUBBLES);
