e267b5deaa512945e57436608c1180e3f2e04d66
donkimber <donkimber@gmail.com>
2017-12-16 12:34:42 -0800

    added a couple of configs for testing some things

--- configs/play/miura.js
+++ configs/play/miura.js
@@ -4,10 +4,8 @@ CONFIG = {
     //'cameraControls': 'Orbit',
     gameOptions: {ambientLightIntensity: 2, headlightIntensity: 3},
     'specs': [
-        {   type: 'Model', name: 'TDRS1',
-            path: 'assets/models/satellites/TDRS1/TDRS1.dae',
-            fitTo: {position: [0,0,0], size: 1}
-        },
+        { type: 'Miura' },
+        { type: 'PointLight', position: [50,50,0], intensity: 3},
     ]
 };
 
