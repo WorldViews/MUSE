fbd0734a2ee955d9391faa8fe29e9d5ed2358ee3
donkimber <donkimber@gmail.com>
2018-01-24 16:51:08 -0800

    config for emotional contagion model

---
+++ configs/play/community.js
@@ -0,0 +1,19 @@
+
+var CONFIG = {
+    //'cameraControls': 'Orbit',
+    cameraControls: {type: 'MultiControls', movementSpeed: .15, keyPanSpeed: .02},
+    gameOptions: {ambientLightIntensity: 2, headlightIntensity: 3},
+    webUI: {type: 'JQControls',
+             //screens: ["mainScreen"],
+          },
+    'program': {
+       duration: 32*60,
+       channels: ['time', 'year', 'narrative', 'spaceStatus'],
+    },
+    //'venue': '/configs/venues/imaginarium.js',
+    nodes: [
+        {type: 'Community'}
+    ]
+};
+
+MUSE.returnValue(CONFIG);
